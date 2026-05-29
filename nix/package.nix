96d1562d2c1e3b68f74690890b1d9058505f42fd
Flower Ornament <31774+flowerornament@users.noreply.github.com>
2026-05-28 20:57:26 -0700

    fix: include git during nix checks

--- nix/package.nix
+++ nix/package.nix
@@ -3,6 +3,7 @@ pkgs.rustPlatform.buildRustPackage {
   pname = "anneal";
   inherit version src;
   cargoLock.lockFile = src + /Cargo.lock;
+  nativeCheckInputs = [ pkgs.git ];
   meta = {
     description = "Convergence assistant for knowledge corpora";
     license = pkgs.lib.licenses.mit;
